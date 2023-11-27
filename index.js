dc02e020118156f614e7e952ed9330c185142d4c
drainthapa <drainthapa@gmail.com>
2023-11-27 16:04:31 +0545

    First Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/ 20231127160311
+// https://plus.codes/api?address=14.917313,-23.511313&email=YOUR_EMAIL_HERE
+
+{
+  "plus_code": {
+    "global_code": "796RWF8Q+WF",
+    "geometry": {
+      "bounds": {
+        "northeast": {
+          "lat": 14.917375000000007,
+          "lng": -23.511250000000018
+        },
+        "southwest": {
+          "lat": 14.91725000000001,
+          "lng": -23.511375000000015
+        }
+      },
+      "location": {
+        "lat": 14.917312500000008,
+        "lng": -23.511312500000017
+      }
+    },
+    "locality": {
+      
+    }
+  },
+  "status": "OK"
+}
